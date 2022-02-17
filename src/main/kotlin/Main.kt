9ff885dfd652d4cf530492bd69174b16fdb04510
Nyam-bura <nyamburaajanee@gmail.com>
2022-02-17 21:19:42 +0300

    add first files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,19 @@
+fun main() {
+    var name = "mitchelle hope"
+    println(name)
+    var age = "21"
+    println(age)
+    var weight = "58"
+    println(weight)
+    var phonenumber  = "0790500842"
+    println(phonenumber)
+    val iskenyan :  Boolean = true
+    println(iskenyan) // output true
+    val isnotkenyan  : Boolean  = false
+    println(isnotkenyan) // output false
+
+
+
+
+
+}
