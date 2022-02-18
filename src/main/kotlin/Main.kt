6d954488686a027383e35aaa1d973eb8cb0d61e5
Nyam-bura <nyamburaajanee@gmail.com>
2022-02-18 21:43:33 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,37 @@
-fun main() {
-    var name = "mitchelle hope"
-    println(name)
-    var age = "21"
-    println(age)
-    var weight = "58"
-    println(weight)
-    var phonenumber  = "0790500842"
-    println(phonenumber)
-    val iskenyan :  Boolean = true
-    println(iskenyan) // output true
-    val isnotkenyan  : Boolean  = false
-    println(isnotkenyan) // output false
+fun  main() {
+    greetings()
+division()
+    addition()
+somethinginteresting()
 
 
 
+}
+
+fun greetings(){
+    var name = "jane"
+    println("hello "+name)
+    division()
+    addition()
+}
+fun division(){
+    var a = 25
+    var b = 4
+    var modulus = a % b
+    println(modulus)
+}
+fun addition(){
+    var y = 10
+    var c = 26
+    var d = 15
+    var x = 12
+    var sum = y+c+d+x
+    println(sum)
 
 
+
+}
+fun somethinginteresting(){
+    var fact = "dancing"
+    println(fact)
 }
